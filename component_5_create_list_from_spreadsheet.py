68bb9696b0d80903b2594dd058771cb432cff9fc
hsiaoy71950 <51727091+hsiaoy71950@users.noreply.github.com>
2020-05-19 11:40:35 +1200

    Update component_5_create_list_from_spreadsheet.py

--- component_5_create_list_from_spreadsheet.py
+++ component_5_create_list_from_spreadsheet.py
@@ -6,10 +6,14 @@ genres = open("02_genres.csv")
 
 csv_genres = csv.reader(genres)
 
-genre_dict= {}
+genre_dict = {}
 
 for row in csv_genres:
-    genre_dict[row[1]] = row[0]
-genre_dict.pop("Source: https://gladreaders.com/types-or-genres-of-books/", None)
+    genre_dict[row[1]] = [row[0], row]
+genre_dict.pop("Source: https://gladreaders.com/types-or-genres-of-books/")
+genre_dict.pop("all")
+x = 1
 for key in genre_dict:
-    print(key, ":", genre_dict[key])
+    print(str(x).zfill(2), ": ", key.capitalize())
+    x += 1
+input("Please select one of the genres above by typing the number or name listed.")
